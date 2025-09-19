87805fa2ddf0167da85fa6c943c18d42513fb4d3
Daniel Carvalho <linksilence@gmail.com>
2025-09-19 17:24:33 -0300

    alteração de classes

--- src/habilidade.java
+++ src/habilidade.java
@@ -1,2 +1,49 @@
 public class habilidade {
+    // Atributos
+    private String nome;
+    private int poder;
+    private tipoPoekmon tipo;
+    private pokemon pokem;
+
+    public habilidade(String nome, int poder, tipoPoekmon tipo, pokemon pokem) {
+        this.nome = nome;
+        this.poder = poder;
+        this.tipo = tipo;
+        this.pokem = pokem;
+    }
+
+    public int usar() {
+        return poder;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPoder(int poder) {
+        this.poder = poder;
+    }
+
+    public void setTipo(tipoPoekmon tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPokemon(pokemon pokem) {
+        this.pokem = pokem;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public tipoPoekmon getTipo() {
+        return tipo;
+    }
+
+    public pokemon getPokemon() {
+        return pokem;
+    }
 }
+
--- src/pokemon.java
+++ src/pokemon.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class pokemon {
     private String nome;
-    private List<tipoPoekmon>tipo;
+    private List<tipoPoekmon>tipo = new ArrayList<>();
     private int vida;
     private treinador treinador;
 
@@ -13,10 +15,7 @@ public class pokemon {
         this.vida=vida;
         this.treinador=treinador;
     }
-    public void setAtaque(pokemon poke)
-    {
-        poke.vida=
-    }
+
     public void setTipo(tipoPoekmon algum) {
         this.tipo.add(algum);
     }
@@ -45,5 +44,52 @@ public class pokemon {
     {
         return treinador;
     }
+
+    public void setAtaque(pokemon alvo, habilidade habilidade) {
+        Random rand = new Random();
+
+        System.out.println(this.getnome() + " usou " + habilidade.getNome() + "!");
+
+        double stabBonus = 1.0;
+        if (this.tipo.contains(habilidade.getTipo())) {
+            stabBonus = 1.5;
+            System.out.println("É um ataque do mesmo tipo! Bônus de 1.5x!");
+        }
+
+        double multiplicadorTipo = 1.0;
+        tipoPoekmon tipoAtaque = habilidade.getTipo();
+
+        if (tipoAtaque.toString().equalsIgnoreCase("FOGO") && alvo.tipo.stream().anyMatch(t -> t.toString().equalsIgnoreCase("GRAMA"))) {
+            multiplicadorTipo = 2.0;
+            System.out.println("É super efetivo!");
+        } else if (tipoAtaque.toString().equalsIgnoreCase("AGUA") && alvo.tipo.stream().anyMatch(t -> t.toString().equalsIgnoreCase("FOGO"))) {
+            multiplicadorTipo = 2.0;
+            System.out.println("É super efetivo!");
+        } else if (tipoAtaque.toString().equalsIgnoreCase("GRAMA") && alvo.tipo.stream().anyMatch(t -> t.toString().equalsIgnoreCase("AGUA"))) {
+            multiplicadorTipo = 2.0;
+            System.out.println("É super efetivo!");
+        }
+
+        if (tipoAtaque.toString().equalsIgnoreCase("FOGO") && alvo.tipo.stream().anyMatch(t -> t.toString().equalsIgnoreCase("AGUA"))) {
+            multiplicadorTipo = 0.5;
+            System.out.println("Não é muito efetivo...");
+        }
+
+        int dano = (int) (habilidade.usar() * stabBonus * multiplicadorTipo);
+        if (dano <= 0) {
+            dano = 1;
+        }
+
+        alvo.vida -= dano;
+        if (alvo.vida < 0) {
+            alvo.vida = 0;
+        }
+
+        System.out.println(alvo.getnome() + " recebeu " + dano + " de dano!");
+        System.out.println("Vida restante de " + alvo.getnome() + ": " + alvo.getVida());
+    }
+
+
+
 
 }
